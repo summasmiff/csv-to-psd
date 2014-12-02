00f71bb5d8af08b405544c41803a9379ec517374
summasmiff <jessicasummersmith@gmail.com>
2014-12-01 20:39:20 -0500

    tidying up

--- csv_updater.rb
+++ csv_updater.rb
@@ -12,17 +12,17 @@ require_relative 'csv-to-pdf.rb'
 @csv = CSV.open("trans-test.csv", "r+") do |csv|
 	csv.find_all do |row|
 		@pdf.each do |i|
-			if ( i === row[1] )
-	      		row << "success"
-	      	end
-	    end
-    end
+			if ( i == row[1] )
+				row << "success"
+			end
+		end
+	end
 end
 
 #output
 CSV.open("trans-test_done.csv", "w", force_quotes: true) do |csv|
-  csv << @csv[0]
-  csv << @csv[1]
-  csv << @csv[2]
+	csv << @csv[0]
+	csv << @csv[1]
+	csv << @csv[2]
 end
 
