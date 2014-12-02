62f511bcd150432af7e4488e9335581ab11cb83e
summasmiff <jessicasummersmith@gmail.com>
2014-12-01 19:30:20 -0500

    updated csv updater to refer to csv-to-pdf

--- csv_updater.rb
+++ csv_updater.rb
@@ -3,30 +3,26 @@ require 'csv'
 require 'psd'
 require 'awesome_print'
 
-require_relative 'csv-to-psd.rb'
+require_relative 'csv-to-pdf.rb'
+
+@pdf = @pdf.scan(/\w+/)
 
 #matchy loop
+#@pdf = our original str
 @csv = CSV.open("trans-test.csv", "r+") do |csv|
 	csv.find_all do |row|
-    	@h.each do |k, v|
-      		row << "success" if( row[1] == @h[k] )
-    	end
+		@pdf.each do |i|
+			if ( i === row[1] )
+	      		row << "success"
+	      	end
+	    end
     end
 end
 
-ap @csv
-
+#output
 CSV.open("trans-test_done.csv", "w", force_quotes: true) do |csv|
   csv << @csv[0]
   csv << @csv[1]
   csv << @csv[2]
 end
 
-#mark CSV with success in third object IF match was found
-#how to add third column to each row?
-
-#split data back out of hash into csv
-#create arr_of_arrays from csv hash
-#split "success" into own index value
-
-#log each array as row
